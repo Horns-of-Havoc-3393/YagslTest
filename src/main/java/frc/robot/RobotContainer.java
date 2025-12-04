433d6d0b32bb9b9f9573173c9fba3bbb1e94df0a
CodeCube314 <rs3codepi314@gmail.com>
2025-12-04 06:19:13 -0800

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,36 @@
+package frc.robot;
+
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class RobotContainer {
+    private final SwerveSubsystem drivebase = new SwerveSubsystem();
+    private final CommandXboxController driver = new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
+
+    public RobotContainer() {
+        configureBindings();
+        drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+    }
+    SwerveInputStream driveAngularVelo = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                              ()-> driver.getLeftY(),
+                                                              ()-> driver.getLeftX())
+                                                              .withControllerRotationAxis(driver::getRightX)
+                                                              .deadband(Constants.stickDeadband)
+                                                              .scaleTranslation(0.8)
+                                                              .allianceRelativeControl(true);
+                                                               
+    SwerveInputStream driveHeading = driveAngularVelo.copy().withControllerHeadingAxis(driver::getRightX,
+                                                                                       driver::getRightY)
+                                                                                       .headingWhile(true);
+
+
+    Command driveFieldOrientedDirectAngle = drivebase.driveFieldOriented(driveHeading);
+    Command driveFieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelo);
+    private void configureBindings() {
+    }
+    public Command getAutonomousCommand() {
+        return null; // fill in later
+    }
+}
